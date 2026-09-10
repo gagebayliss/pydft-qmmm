2acef1f0a6f86eef04960805126c4a235f7e7411
Gage Bayliss <gageisokay@gmail.com>
2026-09-10 12:33:52 -0400

    plumed fix

--- pydft_qmmm/plugins/drude/__init__.py
+++ pydft_qmmm/plugins/drude/__init__.py
@@ -1,16 +1,11 @@
-"""Plugins and helpers for Drude oscillator relaxation."""
 from __future__ import annotations
 
 __all__ = [
     "DrudeData",
-    "DrudeSCFIterator",
     "DrudeSCF",
-    "CompositeSCF",
     "extract_drude_data",
 ]
 
 from .drude_data import DrudeData
 from .drude_data import extract_drude_data
 from .drude import DrudeSCF
-from .drude_solver import DrudeSCFIterator
-from .drude_solver import CompositeSCF
