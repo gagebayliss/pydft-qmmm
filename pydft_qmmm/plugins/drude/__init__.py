0b905dc54987d15f171c9cbe6eb117098b78157c
Gage Daniel Bayliss <gbayliss3@login-phoenix-gnr-4.pace.gatech.edu>
2026-09-10 13:05:39 -0400

    fixed drude bug that broke imports

--- pydft_qmmm/plugins/drude/__init__.py
+++ pydft_qmmm/plugins/drude/__init__.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 __all__ = [
     "DrudeData",
     "DrudeSCF",
+    "DrudeSCFIterator",
+    "CompositeSCF",
     "extract_drude_data",
 ]
 
 from .drude_data import DrudeData
 from .drude_data import extract_drude_data
 from .drude import DrudeSCF
+from .drude_solver import DrudeSCFIterator
+from .drude_solver import CompositeSCF
